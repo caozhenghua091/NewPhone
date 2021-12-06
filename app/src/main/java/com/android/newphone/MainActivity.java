a0c01b6d2d82154a1775bf6c76fa89455ffcc37e
caozhenghua091 <1803830462@qq.com>
2021-12-06 14:40:14 +0800

    fitst commit

---
+++ app/src/main/java/com/android/newphone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.android.newphone;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        
+    }
+}
